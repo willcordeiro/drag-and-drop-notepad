4852464570a356f49d6066f0f07e55787592f20f
willcordeiro <willcordeiro800@gmail.com>
2022-10-24 23:17:39 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { StyleGlobal } from "./styles/Global";
+
+function App() {
+  return (
+    <div className="App">
+      <StyleGlobal />
+    </div>
+  );
+}
+
+export default App;
