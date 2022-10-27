2e1fcceb2429c79b232cd608005ed5e007d8e867
willcordeiro <willcordeiro800@gmail.com>
2022-10-27 15:22:41 -0300

    added Card typescripted component file

--- src/components/List/test.js
+++ src/components/List/test.js
@@ -1,30 +1,6 @@
 const { DragDropContext, Draggable, Droppable } = ReactBeautifulDnd;
 const { useState, useEffect, Fragment } = React;
 
-const TitleBar = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-const Title = styled.h3`
-  padding: 8px;
-  font-size: 1.5em;
-  text-overflow: ellipsis;
-`;
-const Cross = styled.div`
-  padding: 8px 12px;
-  cursor: pointer;
-  text-align: right;
-  color: grey;
-`;
-const CardContainer = styled.div`
-  margin: 8px;
-  border: 1px solid lightgrey;
-  border-radius: 4px;
-  width: 220px;
-  display: flex;
-  flex-direction: column;
-  background-color: white;
-`;
 const TaskList = styled.div`
   padding: 8px;
   background-color: ${(props) =>
@@ -42,87 +18,6 @@ const NewTaskButton = styled.div`
   text-align: right;
   color: grey;
 `;
-
-function Card(props) {
-  const [isAddingNewTask, setIsAddingNewTask] = useState(false);
-  const onSaveTask = (content) => {
-    if (content.trim() !== "") {
-      props.onAddNewTask(content);
-    }
-    setIsAddingNewTask(false);
-  };
-
-  return (
-    <Draggable draggableId={props.card.id} index={props.index}>
-      {(provided) => (
-        <CardContainer
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          id={props.card.id}
-        >
-          <TitleBar>
-            {props.isTitleEditing ? (
-              <EditInput
-                key={props.card.id}
-                value={props.card.title}
-                onSave={props.onSaveTitleEdit}
-                fontSize="1.5em"
-                margin="20px 0 20px 8px"
-              />
-            ) : (
-              <Title
-                onDoubleClick={props.onTitleDoubleClick}
-                {...provided.dragHandleProps}
-              >
-                {props.card.title}
-              </Title>
-            )}
-            <Cross onClick={props.onRemoveCard}>x</Cross>
-          </TitleBar>
-          <Droppable droppableId={props.card.id} type="task">
-            {(provided, snapshot) => (
-              <Fragment>
-                <TaskList
-                  ref={provided.innerRef}
-                  {...provided.droppableProps}
-                  isDraggingOver={snapshot.isDraggingOver}
-                >
-                  {props.tasks.map((task, index) => (
-                    <Task
-                      key={task.id}
-                      task={task}
-                      index={index}
-                      onSaveTaskEdit={(content) =>
-                        props.onSaveTaskEdit(task.id, content)
-                      }
-                      onTaskDoubleClick={() => props.onTaskDoubleClick(task)}
-                      isTaskEditing={props.isTaskEditing(task)}
-                    />
-                  ))}
-                </TaskList>
-                {provided.placeholder}
-              </Fragment>
-            )}
-          </Droppable>
-          <NewTaskBar>
-            {isAddingNewTask ? (
-              <EditInput
-                key="newtask"
-                value=""
-                onSave={onSaveTask}
-                margin="8px"
-              />
-            ) : (
-              <NewTaskButton onClick={() => setIsAddingNewTask(true)}>
-                + New Task
-              </NewTaskButton>
-            )}
-          </NewTaskBar>
-        </CardContainer>
-      )}
-    </Draggable>
-  );
-}
 
 const TaskContainer = styled.div`
   display: flex;
