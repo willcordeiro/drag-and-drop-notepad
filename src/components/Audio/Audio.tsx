ecd2cf3ccaa846fb6f350de469b995fd47c3dad4
willcordeiro <willcordeiro800@gmail.com>
2022-11-03 23:23:36 -0300

    now the musics plays individually

--- src/components/Audio/Audio.tsx
+++ src/components/Audio/Audio.tsx
@@ -1,41 +1,26 @@
-import React, { useState, useEffect, useRef } from "react";
-import { styled, Typography, Slider, Paper, Stack, Box } from "@mui/material";
-
-// #region ------------ ICONS ---------
+import React, { useState, useRef } from "react";
+import { styled, Slider, Paper, Stack, Box } from "@mui/material";
 import VolumeDownIcon from "@mui/icons-material/VolumeDown";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 import VolumeOffIcon from "@mui/icons-material/VolumeOff";
 import VolumeMuteIcon from "@mui/icons-material/VolumeMute";
-
 import PauseIcon from "@mui/icons-material/Pause";
-import FastRewindIcon from "@mui/icons-material/FastRewind";
-import FastForwardIcon from "@mui/icons-material/FastForward";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import SkipNextIcon from "@mui/icons-material/SkipNext";
-import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
-// #endregion ------------ ICONS ---------
+import { audiosData } from "./audioData";
 
-// #region ------- Tracts -------------------------------------------------------
-import fade from "../../assets/audios/sunny day.mp3";
-import enough from "../../assets/audios/summer-rain.mp3";
-
-// #endregion ---------------------------------------------------------------
-
-// #region -------- Styled Components -----------------------------------------
 const Div = styled("div")(({ theme }) => ({
-  height: "100vh",
-  width: "50vw",
-  paddingTop: theme.spacing(6),
+  width: "30%",
 }));
 
 const CustomPaper = styled(Paper)(({ theme }) => ({
-  backgroundColor: "#4c4c4c",
+  backgroundColor: "transparent",
   marginLeft: theme.spacing(6),
   marginRight: theme.spacing(6),
   padding: theme.spacing(2),
+  border: "solid #ece8e1 2px",
 }));
 
-type st = {
+type PSlider = {
   thumbless: any;
   min: any;
   max: any;
@@ -43,8 +28,8 @@ type st = {
   onChange: any;
 };
 
-const PSlider = styled(Slider)<st>(({ theme, ...props }: any) => ({
-  color: "lime",
+const PSlider = styled(Slider)<PSlider>(({ theme, ...props }: any) => ({
+  color: "#ece8e1",
   height: 2,
   "&:hover": {
     cursor: "auto",
@@ -55,61 +40,18 @@ const PSlider = styled(Slider)<st>(({ theme, ...props }: any) => ({
     display: props.thumbless ? "none" : "block",
   },
 }));
-// #endregion ---------------------------------------------------------------
-
-const playlist = [fade, enough];
 
 export default function Player() {
-  const audioPlayer: any = useRef();
-
-  const [index, setIndex] = useState(0);
-
-  const [currentSong] = useState(playlist[index]);
-
+  const audioPlayer: any = useRef([]);
   const [isPlaying, setIsPlaying] = useState(false);
   const [volume, setVolume] = useState(30);
   const [mute, setMute] = useState(false);
 
-  const [elapsed, setElapsed] = useState(0);
-  const [duration, setDuration] = useState(0);
-
-  useEffect(() => {
-    if (audioPlayer) {
-      audioPlayer.current.volume = volume / 100;
-    }
-
-    if (isPlaying) {
-      setInterval(() => {
-        const _duration = Math.floor(audioPlayer?.current?.duration);
-        const _elapsed = Math.floor(audioPlayer?.current?.currentTime);
-
-        setDuration(_duration);
-        setElapsed(_elapsed);
-      }, 100);
-    }
-  }, [volume, isPlaying]);
-
-  function formatTime(time: any) {
-    if (time && !isNaN(time)) {
-      const minutes =
-        Math.floor(time / 60) < 10
-          ? `0${Math.floor(time / 60)}`
-          : Math.floor(time / 60);
-      const seconds =
-        Math.floor(time % 60) < 10
-          ? `0${Math.floor(time % 60)}`
-          : Math.floor(time % 60);
-
-      return `${minutes}:${seconds}`;
-    }
-    return "00:00";
-  }
-
-  const togglePlay = () => {
+  const togglePlay = (i: any) => {
     if (!isPlaying) {
-      audioPlayer.current.play();
+      audioPlayer.current[i].play();
     } else {
-      audioPlayer.current.pause();
+      audioPlayer.current[i].pause();
     }
     setIsPlaying((prev) => !prev);
   };
@@ -117,90 +59,112 @@ export default function Player() {
   function VolumeBtns() {
     return mute ? (
       <VolumeOffIcon
-        sx={{ color: "lime", "&:hover": { color: "white" } }}
+        sx={{ color: "#ece8e1", "&:hover": { color: "#6e38be" } }}
         onClick={() => setMute(!mute)}
       />
     ) : volume <= 20 ? (
       <VolumeMuteIcon
-        sx={{ color: "lime", "&:hover": { color: "white" } }}
+        sx={{ color: "#ece8e1", "&:hover": { color: "#6e38be" } }}
         onClick={() => setMute(!mute)}
       />
     ) : volume <= 75 ? (
       <VolumeDownIcon
-        sx={{ color: "lime", "&:hover": { color: "white" } }}
+        sx={{ color: "#ece8e1", "&:hover": { color: "#6e38be" } }}
         onClick={() => setMute(!mute)}
       />
     ) : (
       <VolumeUpIcon
-        sx={{ color: "lime", "&:hover": { color: "white" } }}
+        sx={{ color: "#ece8e1", "&:hover": { color: "#6e38be" } }}
         onClick={() => setMute(!mute)}
       />
     );
   }
 
   return (
-    <Div>
-      <audio src={currentSong} ref={audioPlayer} muted={mute} />
-      <CustomPaper>
-        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-          {!isPlaying ? (
-            <PlayArrowIcon
-              fontSize={"large"}
-              sx={{ color: "lime", "&:hover": { color: "white" } }}
-              onClick={togglePlay}
-            />
-          ) : (
-            <PauseIcon
-              fontSize={"large"}
-              sx={{ color: "lime", "&:hover": { color: "white" } }}
-              onClick={togglePlay}
-            />
-          )}
-          <Stack
-            direction="row"
-            spacing={1}
-            sx={{
-              display: "flex",
-              justifyContent: "flex-start",
-              width: "25%",
-              alignItems: "center",
-            }}
-          ></Stack>
-          <Stack
-            direction="row"
-            spacing={1}
-            sx={{
-              display: "flex",
-              width: "40%",
-              alignItems: "center",
-            }}
-          ></Stack>
-          <Stack
-            sx={{
-              display: "flex",
-              justifyContent: "flex-end",
-            }}
-          />
-        </Box>
-        <Stack
-          spacing={1}
-          direction="row"
-          sx={{
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
-          <VolumeBtns />
+    <div>
+      {audiosData.map((item, i) => (
+        <Div key={i}>
+          <audio
+            muted={mute}
+            ref={(audioPlayer0): any => (audioPlayer.current[i] = audioPlayer0)}
+          >
+            <source src={item.audio} type="audio/mpeg" />
+          </audio>
+          <CustomPaper>
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+              {item.audioName}{" "}
+              {!isPlaying ? (
+                <PlayArrowIcon
+                  fontSize={"large"}
+                  sx={{
+                    color: "#ece8e1",
+                    "&:hover": { color: "#6e38be" },
+                    cursor: "pointer",
+                  }}
+                  onClick={() => {
+                    togglePlay(i);
+                  }}
+                />
+              ) : (
+                <PauseIcon
+                  fontSize={"large"}
+                  sx={{
+                    color: "#ece8e1",
+                    "&:hover": { color: "#6e38be" },
+                    cursor: "pointer",
+                  }}
+                  onClick={() => {
+                    togglePlay(i);
+                  }}
+                />
+              )}
+              <Stack
+                direction="row"
+                spacing={1}
+                sx={{
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  width: "25%",
+                  alignItems: "center",
+                }}
+              ></Stack>
+              <Stack
+                direction="row"
+                spacing={1}
+                sx={{
+                  display: "flex",
+                  width: "40%",
+                  alignItems: "center",
+                }}
+              ></Stack>
+              <Stack
+                sx={{
+                  display: "flex",
+                  justifyContent: "flex-end",
+                }}
+              />
+            </Box>
+            <Stack
+              spacing={1}
+              direction="row"
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <VolumeBtns />
 
-          <PSlider
-            min={0}
-            max={100}
-            value={volume}
-            onChange={(e: any, v: any) => setVolume(v)}
-            thumbless={undefined}
-          />
-        </Stack>
-      </CustomPaper>
-    </Div>
+              <PSlider
+                min={0}
+                max={100}
+                value={volume}
+                onChange={(e: any, v: any) => setVolume(v)}
+                thumbless={undefined}
+              />
+            </Stack>
+          </CustomPaper>
+        </Div>
+      ))}
+    </div>
   );
 }
--- src/components/Audio/audioData.tsx
+++ src/components/Audio/audioData.tsx
@@ -1,4 +1,4 @@
-import LofiStudyfrom from "../../assets/audios/lofi-study.mp3";
+import LofiStudy from "../../assets/audios/lofi-study.mp3";
 import campfire from "../../assets/audios/campfire.mp3";
 import emptyMindLofi from "../../assets/audios/empty-mind-lofi.mp3";
 import lofiHipHop from "../../assets/audios/lofi-hip-hop.mp3";
@@ -8,10 +8,10 @@ import sunnyDay from "../../assets/audios/sunny day.mp3";
 import summerRain from "../../assets/audios/summer-rain.mp3";
 
 export const audiosData = [
-  { audio: LofiStudyfrom, audioName: "Lofi Study" },
-  { audio: campfire, audioName: "Campfire " },
+  { audio: LofiStudy, audioName: "Lofi Study" },
+  { audio: campfire, audioName: "Campfire" },
   { audio: emptyMindLofi, audioName: "Lofi empty mind" },
-  { audio: lofiHipHop, audioName: "" },
+  { audio: lofiHipHop, audioName: "Lofi Hip Hop" },
   { audio: spiritBlossomLofi, audioName: "Lofi spirit blossom" },
   { audio: street, audioName: "Street" },
   { audio: sunnyDay, audioName: "Sunny Day" },
